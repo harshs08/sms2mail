8732e241f4154e6b55d7aa5d35d5433731535c44
Harsh Singh <harshsin@usc.edu>
2014-03-08 06:16:34 -0800

    added server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: harshsingh
+ * Date: 3/8/14
+ * Time: 6:14 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+// set up ======================================================================
+var express  = require('express');
+var app      = express(); 								// create our app w/ express
+var mongoose = require('mongoose'); 					// mongoose for mongodb
+var port  	 = process.env.PORT || 8080; 				// set the port
+var database = require('./config/database'); 			// load the database config
+
+// configuration ===============================================================
+//mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
+
+app.configure(function() {
+    app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+    app.use(express.logger('dev')); 						// log every request to the console
+    app.use(express.bodyParser()); 							// pull information from html in POST
+    app.use(express.methodOverride()); 						// simulate DELETE and PUT
+});
+
+// routes ======================================================================
+require('./app/routes.js')(app);
+
+// listen (start app with node server.js) ======================================
+app.listen(port);
+console.log("App listening on port " + port);
